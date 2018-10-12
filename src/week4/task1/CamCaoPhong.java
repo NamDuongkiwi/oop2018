921e67c25a1fe0ddc0f71da01739bb90fa571f66
NamDuongkiwi <43265144+NamDuongkiwi@users.noreply.github.com>
2018-10-12 14:59:57 +0700

    Update CamCaoPhong.java

--- src/week4/task1/CamCaoPhong.java
+++ src/week4/task1/CamCaoPhong.java
@@ -1,8 +1,25 @@
 package week4.task1;
 
 class CamCaoPhong extends Cam {
-    public CamCaoPhong(String mausac, String xuatxu, int giatien, int cannang) {
+    // them thuộc tính độ dày của vỏ
+    String day;
+    public CamCaoPhong(String mausac, String xuatxu, int giatien, int cannang, String day) {
         super(mausac, xuatxu, giatien, cannang);
+        this.day = day;
     }
-
+    public String getday(){
+        return this.day;
+    }
+    public voi setday(String day){
+        this.day = day;
+    }
+    //thêm phương thức check chất lượng cam
+    public void check(){
+        if(this.day == "dày");
+            System.out.println("cam đểu");
+        else if(this.day == "mỏng")
+            System.out.println("cam hịn");
+    }
+    
+    
 }
