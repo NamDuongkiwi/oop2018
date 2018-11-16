b262b6a1c92e5eba321a1d1cddb6022536edea94
NamDuongkiwi <43265144+NamDuongkiwi@users.noreply.github.com>
2018-11-16 12:07:38 +0700

    nộp bài tập

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -2,10 +2,10 @@ package week10;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Task1 {
-    public static List<String> getAllFunctions(File path){
+    public static List<String> getAllFunctions1(File path){
         try{
             List<String> result = new ArrayList<String>();
             FileReader fr = new FileReader(path);
@@ -32,9 +32,42 @@ public class Task1 {
         }
     }
 
+    public static List<String> getAllFunctions(File path){
+        List<String> result = new ArrayList<>();
+        List<String> all = new ArrayList<>();
+        try{
+            FileReader fr = new FileReader(path);
+            BufferedReader br = new BufferedReader(fr);
+            String str = null;
+            while((str = br.readLine()) != null){
+                all.add(str);
+            }
+            for(int i = 0; i < all.size(); i++){
+                String temp = new String();
+                if(all.get(i).indexOf("static") > 0 && all.get(i).substring(0,6).equals("    /*") != true){
+                    for(int j = i; all.get(j).equals("    }") != true; j++){
+                        String s = all.get(j);
+                        if(s.indexOf("//") > 0){
+                            int cmt = s.indexOf("//");
+                            s = s.substring(0,cmt);
+                        }
+                        temp += s + "\n";
+                    }
+                    result.add(temp);
+                }
+            }
+            br.close();
+            fr.close();
+            return result;
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
     public static String findFunctionByName(String Name){
         File file = new File("D:\\github\\oop2018\\src\\week9\\Utils.java");
-        List<String> a = getAllFunctions(file);
+        List<String> a = getAllFunctions1(file);
         String result = null;
         for(int i=0; i<a.size();i++){
             String b = a.get(i);
@@ -61,7 +94,7 @@ public class Task1 {
         //TODO: in ra tất cả phương thức
         File file = new File("D:\\github\\oop2018\\src\\week9\\Utils.java");
         List<String> a = getAllFunctions(file);
-        System.out.println("Các Phương thức có trong file là: ");
+        System.out.println("Các Phương thức có trong file là: " );
         for(int i = 0; i < a.size(); i++){
             System.out.println(a.get(i));
         }
